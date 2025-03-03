a03b501140543ec49ebc873b3b33be91cb37474f
dongjae_mac_air <djshin0530@gmail.com>
2025-03-02 21:51:25 -0800

    updated gaussian_process.py

--- active_learning/acquisition.py
+++ active_learning/acquisition.py
@@ -1,9 +1,17 @@
+from botorch.acquisition import PosteriorStandardDeviation, UpperConfidenceBound
 
-def uncertainty_sampling(gp, n_candidates):
-    pass
 
-def upper_confidence_bound(n_candidates):
-    pass
+def uncertainty_sampling_discrete(gp, n_candidates):
+    # suggest n_candidates samples with the highest uncertainty on discrete grid
 
-def expected_improvement(n_candidates):
+    # Instantiate a acquisition function
+    US = PosteriorStandardDeviation(gp)
+
+def upper_confidence_bound_discrete(n_candidates):
+    # suggest n_candidates samples with the highest upper confidence bound on discrete grid
+
+    UCB = UpperConfidenceBound(gp, beta=0.1)
+
+def expected_improvement_discrete(n_candidates):
+    # suggest n_candidates samples with the highest expected improvement on discrete grid
     pass
