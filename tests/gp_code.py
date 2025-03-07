9e4613339c9ed8a054fe6433913dcad11a121aa6
djayshin_slac <djshin0530@gmail.com>
2025-03-07 15:35:40 -0800

    refactor gp_code.py to use home directory for file path

--- tests/gp_code.py
+++ tests/gp_code.py
@@ -1,6 +1,9 @@
+import os
 import active_learning.gaussian_process as gpc
 
-path = "/Users/djayshin/Dropbox/0.Dongjae/04.SUNCAT@SLAC,Standford(2402~)/231128_research/240602_ML_codes/CatNaviGATE/tests/20250228_sheet_for_ML_unique.xlsx"
+home_dir = os.path.expanduser("~")
+path = (home_dir +
+        "/Dropbox/0.Dongjae/04.SUNCAT@SLAC,Standford(2402~)/231128_research/240602_ML_codes/CatNaviGATE/tests/20250228_sheet_for_ML_unique.xlsx")
 # path = "/Users/dongjae/Dropbox/0.Dongjae/04.SUNCAT@SLAC,Standford(2402~)/231128_research/240602_ML_codes/CatNaviGATE/tests/20250228_sheet_for_ML_unique.xlsx"
 
 gp1 = gpc.GaussianProcess()
