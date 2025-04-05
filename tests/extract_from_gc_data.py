335c91c26f324d05485976af60d8d453eff31e6b
djayshin_slac <djshin0530@gmail.com>
2025-04-04 18:51:01 -0700

    update extract_from_gc_data.py to change data path and comment out unused keywords and method calls

--- tests/extract_from_gc_data.py
+++ tests/extract_from_gc_data.py
@@ -6,7 +6,8 @@ import analysis.data_analysis as da
 # Define the home directory and path to data
 home_dir = os.path.expanduser("~")
 path = (home_dir +
-        "/Google Drive/Shared drives/Accelerating Innovations Team Drive/2. Research/8. Data/04 Catalysis Round Robin/01 Round Robin GC Results")
+        # "/Google Drive/Shared drives/Accelerating Innovations Team Drive/2. Research/8. Data/04 Catalysis Round Robin/01 Round Robin GC Results")
+        "/Google Drive/Shared drives/Accelerating Innovations Team Drive/2. Research/8. Data/03 Previous Data/20250404_temporary_data_UCSB+RR")
         # "/Google Drive/Shared drives/Accelerating Innovations Team Drive/2. Research/8. Data/02 GC Experimental Data")
         # "/Dropbox/0.Dongjae/04.SUNCAT@SLAC,Standford(2402~)/231128_research/240602_ML_codes/CatNaviGATE/tests/250328_RR_data")
 # Keywords to exclude
@@ -14,7 +15,7 @@ exclude_keywords = [
     "0p0005", # data with too low Rh mass, likely to be inaccurate
     "(1)",    # data mistakenly uploaded twice
     "PercentLoading_Synthesis_MassLoading_Temperature_Date_Location", # example Excel file
-    "_SLAC",   # data from SLAC
+    # "_SLAC",   # data from SLAC
 ]
 
 # Create an instance of DataForGP
@@ -45,10 +46,10 @@ for column in [
     ]:
     dataset.assign_target_values(
         methods=[
-            'initial value',
-            'final value',
-            'initial slope',
-            'final slope',
+            # 'initial value',
+            # 'final value',
+            # 'initial slope',
+            # 'final slope',
             # 'overall slope',
             'delta'
         ],
@@ -81,8 +82,8 @@ analysis = da.DataAnalysis(dataset=dataset)
 # analysis.plot_tos_data_duplicate(column='CO2 Conversion (%)')
 # analysis.plot_heatmap_snr(vmax=7.53)
 # analysis.plot_heatmap_snr(vmax=2.5)
-analysis.compare_targets_std_dev(target_wise=True, plot_hist=False)
+# analysis.compare_targets_std_dev(target_wise=True, plot_hist=False)
 
 # Export the processed data
-# dataset.export_sheet(unique=True)
-# dataset.export_sheet(unique=False)
+dataset.export_sheet(unique=True)
+dataset.export_sheet(unique=False)
