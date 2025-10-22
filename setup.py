c687cbbc7398499540dc60855a7fbeb0e5a52e09
djayshin_slac <djshin0530@gmail.com>
2025-10-21 20:57:57 -0700

    refactor project structure and update dependencies in setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,18 @@ setup(
     author='Dongjae Shin',
     packages=find_packages(),
     install_requires=[
-        'pandas==2.2.3',
-        # 'botorch==0.14.0',
-        'matplotlib==3.10.3',
+        # 'pandas==2.2.3',
+        # 'mplcursors==0.6',
+        # 'matplotlib==3.9.2',
+        # 'openpyxl==3.1.5',
+        # 'seaborn==0.13.2',
+        # 'statsmodels==0.14.4',
+        # 'ipython==8.25.0',
+        'docx==0.2.4',
+        'mplcursors==0.6',
         'openpyxl==3.1.5',
+        'python-docx==1.1.2',
+        'seaborn==0.13.2',
+        'statsmodels==0.14.4'
     ]
 )
